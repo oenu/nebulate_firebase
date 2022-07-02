a3c23c06285c44e57582b9bf1022015ff488210b
oenu <51684443+oenu@users.noreply.github.com>
2022-07-02 15:41:11 -0400

    added generate table route

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,8 +11,12 @@ import scrapeYoutube = require("./scrape/youtube");
 // Match videos
 import match = require("./channel/match");
 
+// Generate lookup table
+import generateTable = require("./lookup/generateTable");
+
 // Exports
 exports.register = register;
 exports.scrapeNebula = scrapeNebula;
 exports.scrapeYoutube = scrapeYoutube;
 exports.match = match;
+exports.generateTable = generateTable;
